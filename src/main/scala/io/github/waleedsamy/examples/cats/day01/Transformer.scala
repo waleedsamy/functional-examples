7555f4e1388a703ac4972a7bbbdb13de235cfdc5
Waleed Samy <waleedsamygd@gmail.com>
2018-07-22 16:04:27 +0200

    update Validator and introduce Transformer

--- src/main/scala/io/github/waleedsamy/examples/cats/day01/Transformer.scala
+++ src/main/scala/io/github/waleedsamy/examples/cats/day01/Transformer.scala
@@ -7,38 +7,6 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 
-
-object Transformer extends App {
-
-  val f1 = db.findAddressByUserId(1)
-
-  f1 map { a => println(s"findAddressByUserId $a") }
-
-  Await.ready(f1, Duration.Inf)
-}
-
-
-object db {
-  case class User(id: Long, name: String)
-  case class Address(id: Long, userId: Long)
-
-
-  def findUserById(id: Long): OptionT[Future, User] = OptionT {
-    Future(Option(User(1, "A")))
-  }
-
-  def findAddressByUser(user: User):  OptionT[Future, Address] = OptionT {
-    Future(Option(Address(1, 1)))
-  }
-
-  def findAddressByUserId(id: Long): Future[Option[Address]] = (for {
-    user <- findUserById(id)
-    address <- findAddressByUser(user)
-  } yield address).value
-
-}
-
-
 // if you look closely, you’ll realize we don’t need to know anything specific about the “outer” Monad (Future and List from the previous examples).
 // As long as we can map and flatMap over it, we’re fine. On the other hand, see how we destructured the Option?
 // That’s some specific knowledge about the “inner” Monad (Option in this case) that we need to have.
@@ -46,3 +14,35 @@ object db {
 // Here’s the shocking news: we’ve just accidentally invented a Monad Transformer, usually named OptionT! 😱
 
 // OptionT[F, A] is a flat version of F[Option[A]] which is a Monad itself
+// OptionT[List, A], aliased to ListOption[A]
+
+object Transformer extends App {
+
+  val f1 = db.findAddressByUserId(1)
+
+  f1 map { a =>
+    println(s"findAddressByUserId $a")
+  }
+
+  Await.ready(f1, Duration.Inf)
+}
+
+object db {
+  case class User(id: Long, name: String)
+  case class Address(id: Long, userId: Long)
+
+  def findUserById(id: Long): OptionT[Future, User] = OptionT {
+    Future(Option(User(1, "A")))
+  }
+
+  def findAddressByUser(user: User): OptionT[Future, Address] = OptionT {
+    Future(Option(Address(1, 1)))
+  }
+
+  def findAddressByUserId(id: Long): Future[Option[Address]] =
+    (for {
+      user <- findUserById(id)
+      address <- findAddressByUser(user)
+    } yield address).value
+
+}
--- src/main/scala/io/github/waleedsamy/examples/cats/validator/Validator.scala
+++ src/main/scala/io/github/waleedsamy/examples/cats/validator/Validator.scala
@@ -1,66 +1,78 @@
 package io.github.waleedsamy.examples.cats.validator
 
+import cats.data.ValidatedNel
+import cats.syntax.ApplicativeOps
+import cats.implicits._
+import io.github.waleedsamy.examples.cats.validator.FormValidator._
+
 object Validator extends App {
-println(
-  FormValidator.validateForm(
-    username = "fakeUs3rname",
-    password = "password",
-    firstName = "John",
-    lastName = "Doe",
-    age = 15
-  ))
+
+  type ValidationResult[A] = ValidatedNel[DomainValidation, A]
+
+  def validateUserName(userName: String): ValidationResult[String] =
+    if (userName.matches("^[a-zA-Z0-9]+$")) userName.validNel
+    else UsernameHasSpecialCharacters.invalidNel
+
+  def validatePassword(password: String) =
+    if (password.matches(
+          "(?=^.{10,}$)((?=.*\\d)|(?=.*\\W+))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$"))
+      password.validNel
+    else PasswordDoesNotMeetCriteria.invalidNel
+
+  def validateFirstName(firstName: String) =
+    if (firstName.matches("^[a-zA-Z]+$")) firstName.validNel
+    else FirstNameHasSpecialCharacters.invalidNel
+
+  def validateLastName(lastName: String) =
+    if (lastName.matches("^[a-zA-Z]+$")) lastName.validNel
+    else LastNameHasSpecialCharacters.invalidNel
+
+  def validateAge(age: Int) =
+    if (age >= 18 && age <= 75) age.validNel else AgeIsInvalid.invalidNel
+
+  def validateForm(username: String,
+                   password: String,
+                   firstName: String,
+                   lastName: String,
+                   age: Int) = {
+    val l = (validateUserName(username),
+             validatePassword(password),
+             validateFirstName(firstName),
+             validateLastName(lastName),
+             validateAge(age))
+
+    l.mapN(RegistrationData)
+
+  }
+
+  println(
+    validateForm(
+      username = "fakeUs3rname",
+      password = "password",
+      firstName = "John",
+      lastName = "Doe",
+      age = 15
+    ),
+    validateForm(
+      username = "Joe",
+      password = "Passw0r$1234",
+      firstName = "John",
+      lastName = "Doe",
+      age = 21
+    )
+  )
 }
 
-sealed trait FormValidator{
+sealed trait FormValidator {
 
-  final case class RegistrationData(username: String, password: String, firstName: String, lastName: String, age: Int)
-
-  def validateForm(username: String, password: String, firstName: String, lastName: String, age: Int): Either[DomainValidation, RegistrationData] =
-    for {
-      validatedUserName <- validateUserName(username)
-      validatedPassword <- validatePassword(password)
-      validatedFirstName <- validateFirstName(firstName)
-      validatedLastName <- validateLastName(lastName)
-      validatedAge <- validateAge(age)
-    } yield RegistrationData(validatedUserName, validatedPassword, validatedFirstName, validatedLastName, validatedAge)
-
-  def validateUserName(userName: String): Either[DomainValidation, String] =
-    Either.cond(
-      userName.matches("^[a-zA-Z0-9]+$"),
-      userName,
-      UsernameHasSpecialCharacters
-    )
-
-  def validatePassword(password: String): Either[DomainValidation, String] =
-    Either.cond(
-      password.matches("(?=^.{10,}$)((?=.*\\d)|(?=.*\\W+))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$"),
-      password,
-      PasswordDoesNotMeetCriteria
-    )
-
-  def validateFirstName(firstName: String): Either[DomainValidation, String] =
-    Either.cond(
-      firstName.matches("^[a-zA-Z]+$"),
-      firstName,
-      FirstNameHasSpecialCharacters
-    )
-
-  def validateLastName(lastName: String): Either[DomainValidation, String] =
-    Either.cond(
-      lastName.matches("^[a-zA-Z]+$"),
-      lastName,
-      LastNameHasSpecialCharacters
-    )
-
-  def validateAge(age: Int): Either[DomainValidation, Int] =
-    Either.cond(
-      age >= 18 && age <= 75,
-      age,
-      AgeIsInvalid
-    )
+  final case class RegistrationData(username: String,
+                                    password: String,
+                                    firstName: String,
+                                    lastName: String,
+                                    age: Int)
 
   sealed trait DomainValidation {
-    def errorMessage : String
+    def errorMessage: String
   }
 
   case object UsernameHasSpecialCharacters extends DomainValidation {
@@ -68,19 +80,23 @@ sealed trait FormValidator{
   }
 
   case object PasswordDoesNotMeetCriteria extends DomainValidation {
-    def errorMessage: String =  "Password must be at least 10 characters long, including an uppercase and a lowercase letter, one number and one special character."
+    def errorMessage: String =
+      "Password must be at least 10 characters long, including an uppercase and a lowercase letter, one number and one special character."
   }
 
   case object FirstNameHasSpecialCharacters extends DomainValidation {
-    def errorMessage: String =  "First name cannot contain spaces, numbers or special characters."
+    def errorMessage: String =
+      "First name cannot contain spaces, numbers or special characters."
   }
 
   case object LastNameHasSpecialCharacters extends DomainValidation {
-    def errorMessage: String =  "Last name cannot contain spaces, numbers or special characters."
+    def errorMessage: String =
+      "Last name cannot contain spaces, numbers or special characters."
   }
 
   case object AgeIsInvalid extends DomainValidation {
-    def errorMessage: String =  "You must be aged 18 and not older than 75 to use our services."
+    def errorMessage: String =
+      "You must be aged 18 and not older than 75 to use our services."
   }
 }
 
